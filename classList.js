bf2e58256373b597f7212f1d993c6e4823d42b4b
Ruben <contact@rubenmadila.com>
2016-12-06 11:34:14 +0000

    Update classList.js

--- classList.js
+++ classList.js
@@ -11,7 +11,8 @@ function DOMTokenList(el) {
   this.el = el;
   // The className needs to be trimmed and split on whitespace
   // to retrieve a list of classes.
-  var classes = el.className.replace(/^\s+|\s+$/g,'').split(/\s+/);
+  var classes = new String(el.className);
+  classes.replace(/^\s+|\s+$/g,'').split(/\s+/);
   for (var i = 0; i < classes.length; i++) {
     push.call(this, classes[i]);
   }
